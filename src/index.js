ad481f1c9a83af45e6b658ca30a626710703d19a
Jena Stewart <66694416+jenastewart@users.noreply.github.com>
2024-08-18 14:46:55 -0700

    Added beginning JavaScript functionality

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+let losAngelesTime = moment();
+
+losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
+  "h:mm:ss"
+)} <small>${losAngelesTime.format("A")}</small>`;
